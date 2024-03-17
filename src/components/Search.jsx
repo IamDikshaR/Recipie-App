a2499b188891d36317bb35ca5c8c24fb2aef5fa5
diksha <rajdiksha91@gmail.com>
2024-03-18 02:51:39 +0530

    search bar styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import styles from "../css/search.module.css";
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = import.meta.env.VITE_FOOD_API_KEY;
 // console.log(import.meta.env.VITE_FOOD_API_KEY);
 
 export default function Search({ foodData, setFoodData }) {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("pizza");
   useEffect(() => {
     async function fetchFood() {
       const response = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
@@ -13,12 +14,13 @@ export default function Search({ foodData, setFoodData }) {
       //   console.log(data.results);
       setFoodData(data.results);
     }
-    // fetchFood();
+    fetchFood();
   }, [query]);
   return (
     <>
-      <div>
+      <div className={styles.searchContainer}>
         <input
+          className={styles.input}
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           type="text"
