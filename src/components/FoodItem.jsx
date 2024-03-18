6d397b0b2a161fa1868acb424c7b583949949a18
diksha <rajdiksha91@gmail.com>
2024-03-18 18:15:46 +0530

    food item component

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,9 +1,18 @@
+import styles from "../css/foodItem.module.css";
+
 export default function FoodItem({ food }) {
   return (
     <>
-      <h1>{food.title}</h1>
-      <img src={food.image} alt={food.title} />
-      <button>View recipie</button>
+      <div className={styles.itemContainer}>
+        <img className={styles.itemImage} src={food.image} alt={food.title} />
+        <div className={styles.itemContent}>
+          <p className={styles.itemName}>{food.title}</p>
+        </div>
+        {/* <h1>{food.title}</h1> */}
+        <div className={styles.buttonContainer}>
+          <button className={styles.itemButton}>View recipie</button>
+        </div>
+      </div>
     </>
   );
 }
